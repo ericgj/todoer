641cb028833989b5e113c73d880fca97b1e4f118
Eric Gjertsen <ericgj72@gmail.com>
2011-10-02 13:25:33 -0400

    add convenience methods to Todo

--- lib/todoer/todo.rb
+++ lib/todoer/todo.rb
@@ -115,6 +115,12 @@ module Todoer
          @tags << t
        end
        
+       def due_date; self.dates['due']; end
+       def on_date; self.dates['on']; end
+       def start_date; self.dates['start']; end
+       def done_date; self.dates['done']; end
+       def scheduled_date; due_date || on_date; end
+       
        def scheduled?
         !!self.dates['on'] || self.dates['due']
        end
